3ca5d069c0bc4f733504b01e196513600f90d5f2
Bronislava <sinkute.bronislava@gmail.com>
2024-04-13 17:24:08 +0300

    Add tableId in restaurantReceipt

--- Restaurant_System/Models/RestaurantReceipt.cs
+++ Restaurant_System/Models/RestaurantReceipt.cs
@@ -2,9 +2,11 @@
 {
     public class RestaurantReceipt : Receipt
     {
-        public RestaurantReceipt(int id, DateTime date, Order order, string restaurantName)
+        public int TableId { get; private set; }
+        public RestaurantReceipt(int tableId, int id, DateTime date, Order order, string restaurantName)
              : base(id, date, order, restaurantName)
         {
+            TableId = tableId;
         }
     }
 }
