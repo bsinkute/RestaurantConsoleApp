bbeb22d861470fe36dbb896d2f937d80c4bcc648
Bronislava <sinkute.bronislava@gmail.com>
2024-04-15 19:47:09 +0300

    Take order window logic.

--- Restaurant_System/Windows/TakeOrderWindow.cs
+++ Restaurant_System/Windows/TakeOrderWindow.cs
@@ -6,14 +6,32 @@ namespace RestaurantSystem.Windows
     {
         public void Load()
         {
-            Console.Clear();
             List<Table> tables = new List<Table>();
             Table table1 = new Table(1, 5);
             Table table2 = new Table(2, 3);
+            Order someOrder = new Order(1, DateTime.Now, 2, 3);
+            table2.Occupy(someOrder);
             Table table3 = new Table(3, 2);
             tables.Add(table1);
             tables.Add(table2);
             tables.Add(table3);
+            Console.Clear();
+            Console.Write("How many guests should be seated? ");
+            bool isCorectNumber = int.TryParse(Console.ReadLine(), out int numberOfPeople);
+            bool haveFittingTables = tables.Any(table => table.NumberOfSeats >= numberOfPeople);
+
+            while (!isCorectNumber || numberOfPeople < 1 || !haveFittingTables)
+            {
+                if (!haveFittingTables)
+                {
+                    Console.Write("Sorry, we don't have a table that would suit you. if you want, you can split up at different tables.");
+                    Console.ReadLine();
+                    return;
+                }
+                Console.Write("Please enter a positive number: ");
+                isCorectNumber = int.TryParse(Console.ReadLine(), out numberOfPeople);
+            }
+
             Console.WriteLine("Tables:");
             foreach (Table table in tables)
             {
@@ -23,8 +41,10 @@ namespace RestaurantSystem.Windows
             string input = Console.ReadLine();
             bool isValidNumber = int.TryParse(input, out int tableId);
             bool tableExists = tables.Any(table => table.Id == tableId);
+            bool enoughSpace = tables.Any(table => table.Id == tableId && table.NumberOfSeats >= numberOfPeople);
+            bool isTableFree = tables.Any(table => table.Id == tableId && table.IsFree());
 
-            while (!isValidNumber || !tableExists)
+            while (!isValidNumber || !tableExists || !enoughSpace || !isTableFree)
             {
                 if (!isValidNumber)
                 {
@@ -34,12 +54,34 @@ namespace RestaurantSystem.Windows
                 {
                     Console.Write($"Table with Id {tableId} does not exist. Enter a valid table Id: ");
                 }
+                else if (!enoughSpace)
+                {
+                    Console.Write($"Table with Id {tableId} does not have enough seats. Choose a different table Id: ");
+                }
+                else if (!isTableFree)
+                {
+                    Console.Write($"Table with Id {tableId} is occupied. Choose a different table Id: ");
+                }
                 input = Console.ReadLine();
                 isValidNumber = int.TryParse(input, out tableId);
                 tableExists = tables.Any(table => table.Id == tableId);
+                enoughSpace = tables.Any(table => table.Id == tableId && table.NumberOfSeats >= numberOfPeople);
+                isTableFree = tables.Any(table => table.Id == tableId && table.IsFree());
             }
             Table selectedTable = tables.FirstOrDefault(t => t.Id == tableId);
             Console.WriteLine($"Table {tableId} selected. Number of seats: {selectedTable.NumberOfSeats}");
+
+            Order order = new Order(numberOfPeople, DateTime.Now, selectedTable.Id, selectedTable.NumberOfSeats);
+            selectedTable.Occupy(order);
+
+            Console.Clear();
+            Console.WriteLine("Tables:");
+            foreach (Table table in tables)
+            {
+                Console.WriteLine(table);
+            }
+            Console.ReadLine();
+            
         }
 
     }
