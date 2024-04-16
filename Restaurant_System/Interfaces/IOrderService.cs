d88a2f5383a635170f2f767664c70e3ad108a5a5
Bronislava <sinkute.bronislava@gmail.com>
2024-04-16 11:33:23 +0300

    add order service interface, order and table services

---
+++ Restaurant_System/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantSystem.Models;
+
+namespace RestaurantSystem.Interfaces
+{
+    public interface IOrderService
+    {
+        List<Order> GetOrder();
+        void SaveOrder(List<Order> order);
+    }
+}
---
+++ Restaurant_System/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantSystem.Infrastructure;
+using RestaurantSystem.Interfaces;
+using RestaurantSystem.Models;
+
+namespace RestaurantSystem.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IDataService<List<Order>> _orderDataService;
+        public OrderService(IDataService<List<Order>> orderDataService)
+        {
+            _orderDataService = orderDataService;
+        }
+        public List<Order> GetOrder()
+        {
+            List<Order> order = _orderDataService.ReadJson() ?? new List<Order>();
+            return order;
+        }
+
+        public void SaveOrder(List<Order> order)
+        {
+            _orderDataService.WriteJson(order);
+        }
+    }
+}
